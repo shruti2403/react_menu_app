84fa647fcf6c93e0860d00178a79fc1de5c024bd
shruti2403 <shrutiverma2403@gmail.com>
2023-12-11 20:26:30 +0530

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Navbar.css'
 
+import logo from '../Assets/logo.jpeg'
+import cart from '../Assets/cart.png'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="nav-logo">
+        <img src={logo} alt=""/>
+        <p></p>
+      </div>
+
+      <ul className="nav-menu">
+      <li>Home</li>
+      <li>Type</li>
+      <li>search</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart} alt=""/>
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   )
 }
