1b0509e78d7c5ca66d0705bcda1da3d43f652c74
shruti2403 <shrutiverma2403@gmail.com>
2023-12-19 22:04:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      
       <Navbar/>
+      <Routes>
+      <Route path='/' element={<cart/>}/>
+      <Route path='/LoginSignup' element={<LoginSignup/>}/>
+      <Route path='/menu' element={<Menu/>}/>
+      {/* <Route path='/LoginSignup' element={<LoginSignup/>}/> */}
+      </Routes>
+
+      </BrowserRouter>      
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.jpeg'
-import cart from '../Assets/cart.png'
+import cart1 from '../Assets/cart1.jpg'
+import home_page from '../Assets/home_page.jpg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-        <img src={logo} alt=""/>
+        <img src={logo} alt="" width="150" height="100" />
         <p></p>
       </div>
 
       <ul className="nav-menu">
       <li>Home</li>
-      <li>Type</li>
-      <li>search</li>
+      <li><div class="dropdown">
+  <button class="dropbtn">Type</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div></li>
+      <li><div class="topnav"> <input type="text" placeholder="Search.."/></div></li>
       </ul>
 
       <div className="nav-login-cart">
+        <Link to='login'></Link>
         <button>Login</button>
-        <img src={cart} alt=""/>
+        <img src={cart1} alt="" width="45" 
+     height="45"/>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
