109af0f7585e1c0f94391d92cbc3f3b76e469f5d
shruti2403 <shrutiverma2403@gmail.com>
2023-12-23 00:06:20 +0530

    commit

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Popular.css'
+import Item from '../Item/Item'
+const Popular = () => {
+  return (
+    <div className='popular'>
+      
+    </div>
+  )
+}
+
+export default Popular
